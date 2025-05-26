4d97a8ed989d556dfefddd101b77bacaf3c54567
Shan Amin <robin_zill@yahoo.com>
2025-05-26 18:51:56 +0500

    Created the Friend Request backend

---
+++ convex/_utils.ts
@@ -0,0 +1,13 @@
+import { MutationCtx, QueryCtx } from "./_generated/server";
+
+type Props = {
+  ctx: QueryCtx | MutationCtx;
+  clerkId: string;
+};
+
+export const getUserByClerkId = async ({ ctx, clerkId }: Props) => {
+  return await ctx.db
+    .query("users")
+    .withIndex("by_clerkId", (q) => q.eq("clerkId", clerkId))
+    .unique();
+};
--- convex/schema.ts
+++ convex/schema.ts
@@ -6,10 +6,6 @@ import { v } from "convex/values";
 // app will continue to work.
 // The schema provides more precise TypeScript types.
 export default defineSchema({
-  numbers: defineTable({
-    value: v.number(),
-  }),
-
   // Users Table
   users: defineTable({
     username: v.string(),
@@ -19,4 +15,12 @@ export default defineSchema({
   })
     .index("by_email", ["email"])
     .index("by_clerkId", ["clerkId"]),
+
+  //Requests Table
+  requests: defineTable({
+    sender: v.id("users"),
+    receiver: v.id("users"),
+  })
+    .index("by_receiver", ["receiver"])
+    .index("by_receiver_sender", ["receiver", "sender"]),
 });
